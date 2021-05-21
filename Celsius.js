21119c81b3b0d94d1bb4adc4638c3f87936d88a7
Oluwagbamila Boluwatife <boluwatt@gmail.com>
2021-05-21 16:24:55 +0100

    first commit

---
+++ Celsius.js
@@ -0,0 +1,39 @@
+function convertFahrToCelsius (f) {
+    if(typeof f == 'number') {
+        let celsius = ((f-32) * (5/9))
+        return (celsius.toFixed(4))
+    }
+
+    else if (typeof f == 'object') {
+        if (Array.isArray(f)) {
+            console.log('this is an array')
+            return ('${f} is not a valid number but an array')
+        }
+        else {
+            console.log('this is an object');
+            output = ('${JSON.stringify(f)} is not a valid number but a ${typeof f}')
+            return output
+        }
+    }
+
+    else if (typeof f == 'string') {
+        console.log('')
+        //let fNumber = Number(f); 
+        return ('this is not a valid number but a string')  
+
+        if(isNaN(f)) {
+            console.log('this is not a number')
+            return ('${f} is not a valid number but a ${typeof f}')
+        }
+
+        else {
+            let celsius = ((f-32) * (5/9))
+            return (celsiustoFixed(4)) 
+        }
+    }
+
+    else {
+        console.log('failure')
+    }
+}
+
---
+++ checkYuGiOh.js
@@ -0,0 +1,31 @@
+function checkYuGiOh (n) {
+    if(typeof n == 'number') {
+        let 
+    }
+
+    else if (typeof n == 'object') {
+        if (Array.isArray(n)) {
+            console.log('this is an array')
+            return ('${n} is not a valid number but an array')
+        }
+        else {
+            console.log('this is an object');
+            output = ('${JSON.stringify(n)} is not a valid number but a ${typeof f}')
+            return output
+        }
+    }
+
+    else if (typeof f == 'string') {
+        console.log('')
+        return ('this is not a valid number but a string')  
+
+        if(isNaN(f)) {
+            console.log('this is not a number')
+            return ('${n} is not a valid number but a ${typeof n}')
+        }
+    }
+
+    else {
+        console.log('failure')
+    }
+}
